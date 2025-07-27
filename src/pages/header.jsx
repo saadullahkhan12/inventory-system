ef62e4c1009ff1d612f2ec21c05dcd7a903dc0ca
saadullahkhan12 <muhmmadsaadullah093@gmail.com>
2025-07-27 18:44:42 +0500

    header chaneg

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -1,40 +1,32 @@
-  import * as React from 'react';
-  import Box from '@mui/material/Box';
-  import Avatar from '@mui/material/Avatar';
+import * as React from 'react';
+import { Tabs, Tab, Box } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
 
-  import IconButton from '@mui/material/IconButton';
-  import Typography from '@mui/material/Typography';
-  
-  
-  import { Link } from 'react-router-dom';
+function Header() {
+  const location = useLocation();
+  const [value, setValue] = React.useState(location.pathname);
 
+  React.useEffect(() => {
+    setValue(location.pathname);
+  }, [location.pathname]);
 
+  return (
+    <Box className="w-full flex justify-center py-4 bg-white shadow-md rounded-xl">
+      <Tabs
+        value={value}
+        onChange={(event, newValue) => setValue(newValue)}
+        textColor="primary"
+        indicatorColor="primary"
+        variant="scrollable"
+        scrollButtons="auto"
+        className="max-w-screen-md w-full"
+      >
+        <Tab label="Inventory" value="/Inventory" component={Link} to="/Inventory" />
+        <Tab label="Income" value="/Icome" component={Link} to="/Icome" />
+        <Tab label="Slips" value="/Slips" component={Link} to="/Slips" />
+      </Tabs>
+    </Box>
+  );
+}
 
-  function Hedaer() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-    return (
-      <>
-      
-        <Box  sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-            
-        <Link to="/Inventory" > <Typography className='menu-text' sx={{ minWidth: 100 }}>Inventory</Typography> </Link>
-        <Link to="/Icome" > <Typography className='menu-text' sx={{ minWidth: 100 }}>Icome</Typography> </Link>
-        <Link to="/Slips" > <Typography className='menu-text' sx={{ minWidth: 100 }}>Slip</Typography> </Link>
-       
-            
-        
-         
-        </Box>
-      
-      </>
-    )
-  }
-
-  export default Hedaer
+export default Header;
