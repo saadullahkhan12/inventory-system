ff83d0bc2551db4751d1de352d0a0c5d4d233594
saadullahkhan12 <muhmmadsaadullah093@gmail.com>
2025-10-31 12:47:53 +0500

    backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://inventory-system-back-end.onrender.com/api';
 
 export const axiosApi = axios.create({
   baseURL: API_BASE_URL,
